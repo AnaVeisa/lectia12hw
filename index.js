3c2e69f339ed2bd8f9fb4015383d3e4be4e7eee6
AnaVeisa <131899856+AnaVeisa@users.noreply.github.com>
2024-03-04 18:13:46 +0200

    javascript

---
+++ index.js
@@ -0,0 +1,40 @@
+var testVariable = 10
+let letVariable = 9
+const testConstant = 16
+
+testVariable = 20
+
+letVariable = 50
+
+let str = "alo"
+
+let test
+
+let testBigInt = BigInt(1098989)
+
+let carName = 'Volvo'
+
+let num1 = 100
+let num2 = 50
+//num++
+//num1--
+//num1 = num1 - 1
+
+//console.log(num1 --)
+
+//console.log(num1 ++)
+//console.log(++ num1)
+
+//num1 = num1 + 5
+//num1 += num2
+num1 -= 2
+//console.log(num1 **3)
+
+const bool = true
+
+//console.log(!bool)
+
+const str = 'str'
+const str1 = 'ing'
+
+console.log(str + str1)
